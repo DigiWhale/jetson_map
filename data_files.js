ba2cb0de94c16367d65b4fe5acfaef60fad4c1c3
digiwhale <digiwhale.humphrey@gmail.com>
2021-12-03 15:39:15 -0500

    1

--- data_files.js
+++ data_files.js
@@ -1,13 +1,16 @@
 const dataFolder = '/home/msrs/msrs_jetson/log_maps';
 const fs = require('fs');
+const csv2geojson = require('csv2geojson');
 
 const getDataFiles = () => {
   fs.readdir(dataFolder, (err, files) => {
     files.forEach(file => {
       if (!file.includes('._')) {
         console.log(file);
+        var geoJson = csv2geojson.csv2geojson(dataFolder + file, function(err, data) {
+          console.log(data)
+        });
       }
-      // console.log(file);
     });
   });
 };

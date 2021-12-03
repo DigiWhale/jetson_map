79ed7c4910305559d0b8e68f8c35ca87e0f4d21d
digiwhale <digiwhale.humphrey@gmail.com>
2021-12-03 13:54:18 -0500

    1

---
+++ config/db.js
@@ -0,0 +1,34 @@
+const redis = require('ioredis');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env' });
+host = process.env.REDIS_HOST;
+port = process.env.REDIS_PORT;
+password = process.env.REDIS_PASSWORD;
+database = process.env.REDIS_DB;
+
+const connectDB = () => {
+  try {
+    const client = new redis({port: port, host: host, password: password, db: database});
+    client.on('connect', () => {
+      console.log('Redis connected.');
+    });
+    client.on('error', err => {
+      console.log('Error ' + err);
+    });
+  } catch (err) {
+      console.log(err);
+  }
+};
+
+const getValue = (keyword) => {
+  console.log('Connecting to redis');
+  try {
+        client.get(keyword, function(err, reply) {
+          console.log(reply);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+module.exports = connectDB, getValue;
---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load env variables
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Enable CORS
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));

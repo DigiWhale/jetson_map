9e752d1fc25c88a3f288fc96b164f4781edeea66
digiwhale <digiwhale.humphrey@gmail.com>
2021-12-03 15:44:35 -0500

    1

--- data_files.js
+++ data_files.js
@@ -5,9 +5,9 @@ const csv2geojson = require('csv2geojson');
 const getDataFiles = () => {
   fs.readdir(dataFolder, (err, files) => {
     files.forEach(file => {
-      if (!file.includes('._')) {
+      if (!file.includes('._') && file.includes('.csv')) {
         console.log(dataFolder + file);
-        var geoJson = csv2geojson.csv2geojson(dataFolder + file, function(err, data) {
+        var geoJson = csv2geojson.csv2geojson(dataFolder + file, {latfield: 'rpi_lat',lonfield: 'rpi_lon',delimiter: ','}, function(err, data) {
           console.log(data)
         });
       }

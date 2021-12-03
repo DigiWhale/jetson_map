91f4cabdfde6af9fdc2f3d80b235d8a74f7032df
digiwhale <digiwhale.humphrey@gmail.com>
2021-12-03 15:48:46 -0500

    1

--- data_files.js
+++ data_files.js
@@ -8,8 +8,8 @@ const getDataFiles = () => {
       if (!file.includes('._') && file.includes('.csv')) {
         console.log(dataFolder + file);
         var geoJson = csv2geojson.csv2geojson(dataFolder + file, {latfield: 'rpi_lat',lonfield: 'rpi_lon',delimiter: ','}, function(err, data) {
-          console.log(data, err)
         });
+        console.log(geoJson);
       }
     });
   });
